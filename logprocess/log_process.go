d4ce58dd42ada7745649449f6fa4753aec0ba889
MingCrash <553753991@qq.com>
2019-10-16 17:37:36 +0800

    ddd

--- logprocess/log_process.go
+++ logprocess/log_process.go
@@ -3,10 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/influxdata/influxdb1-client/v2"
-	"image"
+	client "github.com/influxdata/influxdb1-client/v2"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"regexp"
 	"strings"
@@ -14,7 +14,7 @@ import (
 )
 
 type Reader struct {
-	logpath string
+	logpath 	string
 }
 
 type Writer struct {
@@ -22,10 +22,10 @@ type Writer struct {
 }
 
 type LogProcess struct {
-	rc chan []byte
-	wc chan LogMessage
-	logreader Reader
-	infwriter Writer
+	rc chan 	[]byte
+	wc chan 	LogMessage
+	logreader 	Reader
+	infwriter 	Writer
 }
 
 type Read interface {
@@ -36,9 +36,10 @@ type Write interface {
 	WirteToInfluxDB()
 }
 
+//日志信息
 type LogMessage struct {
-	Time 						time.Time
-	Level, TraceId, Url 		string
+	Time 								time.Time
+	Level, TraceId, Url, statCode 		string
 }
 
 func (r *Reader) ReadFromFile(rc chan []byte)  {
@@ -55,10 +56,12 @@ func (r *Reader) ReadFromFile(rc chan []byte)  {
 	for {
 		line, err := rd.ReadBytes('\n')
 		if err == io.EOF {
+			statusMonitorChan <- ErrorTypeNum
 			time.Sleep(500*time.Millisecond)
 			continue
 		}
 		if err != nil {
+			statusMonitorChan <- ErrorTypeNum
 			panic(fmt.Sprintf("log file readed failture:%s", err.Error()))
 		}
 		rc <- line[:len(line)-1]
@@ -68,23 +71,25 @@ func (r *Reader) ReadFromFile(rc chan []byte)  {
 func (l *LogProcess) Process()  {
 	//解析模块
 	loc,err := time.LoadLocation("PRC")
-	rg, err := regexp.Compile("t=([\\d+\\:\\-\\s]+)[^\n]*Level=([a-z]+)[^\n]*TraceId=(\\d+)[^\n]*Url=([^`]+)")
+	rg, err := regexp.Compile("t=([\\d+\\:\\-\\s]+)[^\n]*Level=([a-z]+)[^\n]*TraceId=(\\d+)[^\n]*Url=([^`]+)[^\n]*code=(\\d+)")
 	for	data := range l.rc{
 		if err != nil {
+			statusMonitorChan <- ErrorTypeNum
 			continue
 		}
 		list := rg.FindStringSubmatch(string(data))
-		if len(list) < 5 {
+		if len(list) < 6 {
+			statusMonitorChan <- ErrorTypeNum
 			continue
 		}
 		msg := LogMessage{}
-		//msg.Timelocl = time.Now()
 		msg.Time,_ = time.ParseInLocation("2006-01-02 15:04:05", list[1], loc)
 		msg.Level = list[2]
 		msg.TraceId = list[3]
 		msg.Url = list[4]
-
+		msg.statCode = list[5]
 		l.wc <- msg
+		statusMonitorChan <- HandlelineTypeNum
 	}
 }
 
@@ -92,46 +97,49 @@ func (w *Writer) WirteToInfluxDB(wc chan LogMessage) {
 	//写入 InfluxDB 时序数据库
 
 	// Create a new HTTPClient
-	//http://localhost:8086&zhuzhiming&suzuki
-	sn := strings.Split(w.influxDBDsn,"&")
+	sn := strings.Split(w.influxDBDsn,"@")
 	cle, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:     	sn[0],
 		Username: 	sn[1],
 		Password: 	sn[2],
 	})
 	if err != nil {
+		statusMonitorChan <- ErrorTypeNum
 		panic("Error creating InfluxDB Client: ")
 	}
 	defer cle.Close()
 
 	// Close client resources
-	if err := cle.Close(); err != nil {
-		fmt.Println("Error closing client: ", err.Error())
-	}
+	//if err := cle.Close(); err != nil {
+	//	fmt.Println("Error closing client: ", err.Error())
+	//}
 
 	for data := range wc{
-		//println(data.Url,data.TraceId,data.Level,data.Time.Format("2006-01-02 15:04:05"),data.Timelocl.Format("2006-01-02 15:04:05"))
 		// Create a point
 		tags := map[string]string{
-			"Url": data.Url,
-			"Level":   data.Level,
-			"TraceId": data.TraceId,
+			"Url": 		data.Url,
+			"Level":   	data.Level,
+			"Code":   	data.statCode,
 		}
 		fields := map[string]interface{}{
-			"Time":   data.Time,
-
+			"Time":   	data.Time,
+			"TraceId": 	data.TraceId,
 		}
-		pt, err := client.NewPoint("mallorder", tags, fields, time.Now())
+		fmt.Println(tags, fields)
+
+		pt, err := client.NewPoint("Mallorder", tags, fields, time.Now())
 		if err != nil {
+			statusMonitorChan <- ErrorTypeNum
 			log.Fatal(err)
 		}
 
 		// Create a new point batch  创建批量注入
 		bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-			Database:  MyDB,
+			Database:  "logprocess",
 			Precision: "s",
 		})
 		if err != nil {
+			statusMonitorChan <- ErrorTypeNum
 			fmt.Println("Error creating NewBatchPoints: ", err.Error())
 		}
 
@@ -140,7 +148,76 @@ func (w *Writer) WirteToInfluxDB(wc chan LogMessage) {
 
 		// Write the batch
 		if err := cle.Write(bp); err != nil {
+			statusMonitorChan <- ErrorTypeNum
 			log.Fatal(err)
 		}
 	}
+}
+
+
+//=====================================================================================================================
+
+const (
+	ErrorTypeNum = 0
+	HandlelineTypeNum = 1
+)
+
+var statusMonitorChan = make(chan int ,100)
+
+//日志系统监控信息
+type Systeminfo struct {
+	Handleline  	int           `json:"handleline"`		//总处理日志行数
+	Tps         	float64       `json:"tps"`				//监控系统吞吐量
+	Readchan    	int           `json:"readchan"` 		//当前读日志缓存数(read channel长度)
+	Writechan    	int           `json:"writechan"`		//当前读日志缓存数(write channel长度)
+	RunTotletime	string        `json:"run_totletime"`	//日志系统运行总时间
+	ErrTotal 		int 		  `json:"err_total"`		//处理错误总数
+}
+
+type SystemMonitor struct {
+	StartTime  		time.Time
+	info			Systeminfo
+}
+
+
+
+func (sm *SystemMonitor) Monitor(lp *LogProcess)  {
+	//日志系统监控器模块
+	var subline int
+
+	//日志系统监控信息收集协程
+	go func() {
+		for sy := range statusMonitorChan {
+			switch sy {
+			case ErrorTypeNum:
+				sm.info.ErrTotal ++
+			case HandlelineTypeNum:
+				sm.info.Handleline ++
+			}
+		}
+	}()
+
+	//每五秒
+	ticker := time.NewTicker(5*time.Second)
+	go func() {
+		for {
+			lasttmp := sm.info.Handleline
+			//此处在等待channel中的信号，因此执行此段代码时会阻塞5秒
+			_,ok := <-ticker.C   	//读取空 channel 会阻塞
+			if !ok {
+				log.Fatal("Error:channel has been closed")
+			}
+			subline = sm.info.Handleline - lasttmp
+		}
+	}()
+
+	//创建http server
+	http.HandleFunc("/monitor", func(writer http.ResponseWriter, request *http.Request) {
+		sm.info.RunTotletime = string(time.Now().Sub(sm.StartTime))
+		sm.info.Readchan = len(lp.rc)
+		sm.info.Writechan = len(lp.wc)
+		sm.info.Tps = float64(subline/5)
+	})
+
+	_ = http.ListenAndServe(":9091", nil)  //ListenAndServe 有阻塞作用
 }
--- logprocess/main.go
+++ logprocess/main.go
@@ -1,41 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"flag"
+	"runtime"
 )
 
 func main()  {
-	//runtime.GOMAXPROCS(runtime.NumCPU())
-	//rd := Reader{
-	//	logpath:"MallOrder-log.txt",
-	//}
-	//wd := Writer{
-	//	influxDBDsn:"name&pass",
-	//}
-	//
-	//lp := &LogProcess{
-	//	rc: make(chan []byte),
-	//	wc: make(chan LogMessage),
-	//	logreader: rd,
-	//	infwriter: wd,
-	//}
-	//go lp.logreader.ReadFromFile(lp.rc)
-	//go lp.Process()
-	//go lp.infwriter.WirteToInfluxDB(lp.wc)
-	//
-	//time.Sleep(30*time.Second)
+	var logpath, influxsn string
+	flag.StringVar(&logpath, "logpath", "MallOrder-log.log","set log path of target for reading")
+	flag.StringVar(&influxsn, "influxsn", "http://localhost:8086@zhuzhiming@suzuki","set influDB's host and client in format:http://localhost:8086@zhuzhiming@suzuki")
+	flag.Parse()
 
-	//loc,_ := time.LoadLocation("PRC")
-	//fmt.Println(time.Now().Format("2006/01&02 15:04:05"))
-	//fmt.Println(time.Now().Format(time.RFC822))
-	////fmt.Println(time.Parse("2016-01-02 15:04:05", "2018-04-23 12:24:51"))
-	//fmt.Println(time.ParseInLocation("2006-01-02 15:04:05", "2012-05-11 14:06:06", loc))
-	//fmt.Println(time.Now().Date())
-	//fmt.Println(time.Now().Clock())
-	//fmt.Println(time.Now().Second())
-	//fmt.Println()
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	rd := Reader{
+		logpath:"MallOrder-log.log",
+	}
+	wd := Writer{
+		influxDBDsn:"http://192.168.0.48:8086@zhuzhiming@suzuki",
+	}
 
-	css := "http://localhost:8086&zhuzhiming&suzuki"
-	fmt.Println(strings.Split(css,"&"))
+	lp := &LogProcess{
+		rc: make(chan []byte,100),   		//添加缓存，缓冲不同协程的处理速度差问题
+		wc: make(chan LogMessage,100),  	//添加缓存，缓冲不同协程的处理速度差问题
+		logreader: rd,
+		infwriter: wd,
+	}
+	sy := &SystemMonitor{}
+	go lp.logreader.ReadFromFile(lp.rc)
+	go lp.Process()
+	go lp.infwriter.WirteToInfluxDB(lp.wc)
+
+	sy.Monitor(lp)
+
+	//ticker := time.NewTicker(5*time.Second)
+	//go func() {
+	//	for {
+	//		//此处在等待channel中的信号，因此执行此段代码时会阻塞5秒
+	//		fmt.Println("未接受信号,等待")
+	//		<-ticker.C
+	//		fmt.Println("已接受信号，处理定时函数")
+	//	}
+	//}()
+	//
+	//time.Sleep(11*time.Second)
 }
