af7f30b198030dee5427b37526f348cccad6e276
MingCrash <553753991@qq.com>
2019-10-22 17:51:11 +0800

    fff

--- InstantMessage/main.go
+++ InstantMessage/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
-	"strconv"
 )
 
 const (
@@ -11,35 +14,70 @@ const (
 )
 
 type loginresp struct {
-	Code  int
-	Msg   string
-	data  interface{}     //interface{} 在golang语言中表示任何类型
+	Code  		int
+	Msg   		string
+	Data  		interface{}    	 //interface{} 在golang语言中表示任何类型
 }
 
-
+func Resp(code int,msg string,data interface{},writer http.ResponseWriter){
+	h := &loginresp{
+		Code:code,
+		Msg:msg,
+		Data:data,
+	}
+	b,err := json.Marshal(h)
+	if err != nil {
+		fmt.Println("json编码出错")
+	}
+	_, err = writer.Write(b)
+	if err != nil {
+		fmt.Println("写入器出错")
+	}
+}
 
 func userLogin(writer http.ResponseWriter, request *http.Request)  {
 		writer.Header().Set("`Content`-Type","json/xml")
-		//_ = request.ParseForm()						//读取参数前需要解析
-		//mobile := request.PostForm.Get("mobile")
+		//_ = request.ParseForm()
+		//mobile := request.PostForm.Get("mobile")		//读取参数前需要解析
 		//passwd := request.PostForm.Get("passwd")
 
 		mobile := request.PostFormValue("mobile")	//调用时，已自动解析参数
 		passwd := request.PostFormValue("passwd")
 
-		var body string
+
 		if (mobile == "13697413574" && passwd == "123456"){
 			writer.WriteHeader(success_status)
-			body = `{"code":`+strconv.Itoa(success_status)+`,"msg":"Login success","data":{"id":100001,"token":"test"}}`
+			Resp(success_status,"Login success",`{"id":100001,"token":"test"}`,writer)
 		}else {
 			writer.WriteHeader(notresourcefound_status)
-			body = `{"code":`+strconv.Itoa(notresourcefound_status)+`,"msg":"Not resource found","data":{"id":none,"token":"test"}}`
+ 			Resp(notresourcefound_status,"Not resource found",`{"id":none,"token":"test"}`,writer)
 		}
-		_, _ = writer.Write([]byte(body))
 }
 
 func main() {
+	//处理 通过API访问 的函数
 	http.HandleFunc("/user/login",userLogin)
+
+	//提供静态资源目录支持
+	http.Handle("/static/css/",http.FileServer(http.Dir("./static/css")))   //pattern不是胡乱起名，需要根据具体目录地址
+	http.Handle("/static/fonts/",http.FileServer(http.Dir("./static/fonts")))   //pattern不是胡乱起名，需要根据具体目录地址
+	http.Handle("/static/images/",http.FileServer(http.Dir("./static/images")))   //pattern不是胡乱起名，需要根据具体目录地址
+
+	//使用template模板渲染
+	http.HandleFunc("/user/login.html", func(w http.ResponseWriter, r *http.Request) {
+		//template解析
+		tpl, err := template.ParseFiles("view/login.html")
+		if err != nil {
+			//打印并直接退出
+			log.Fatal("template ParseFiles Failture" + err.Error())
+		}
+		err = tpl.ExecuteTemplate(w, "/user/login/login.html", nil)
+
+		if err != nil {
+			//打印并直接退出
+			log.Fatal("Execute Template Failture"+err.Error())
+		}
+	})
 
 	_ = http.ListenAndServe(":9090", nil)
 }
