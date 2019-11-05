1ea53fb19ae422e5d293eab5d05b615d7815ccb3
zhuzhiming <553753991@qq.com>
2019-11-05 08:05:57 +0800

    kk

--- InstantMessage/controller/chat.go
+++ InstantMessage/controller/chat.go
@@ -8,12 +8,18 @@ import (
 	"strconv"
 )
 
+type ConnNode struct {
+	Conn 		*websocket.Conn 	`json:"conn"`
+	//将结点接收到的可能性并行信息转换成串行信息,
+	DataQueue	chan []byte 		`json:"data_queue"`
+	setGroup
+}
+
+//本核心在于形成userid和Node的映射关系
 
 //	127.0.0.1/chat?id=xxx&token=xxx
 func Chat(writer http.ResponseWriter, request *http.Request)  {
 	//检查接入是否合法
-	//toverified_id := request.PostFormValue("id")
-	//toverified_token := request.PostFormValue("token")
 	query := request.URL.Query()
 	id := query.Get("id")
 	toverified_token := query.Get("token")
@@ -21,14 +27,21 @@ func Chat(writer http.ResponseWriter, request *http.Request)  {
 	if err != nil {
 		unit.RespFail(writer,err)
 	}
+	isvalid, err := CheckToken(toverified_id,toverified_token)
+	if isvalid == false || err != nil {log.Println(err.Error())}
 
-	_, _ = websocket.Upgrader{
+	//把 http 请求升级为长连接的 WebSocket
+	conn, err := websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
-			isvalid,_ := CheckToken(toverified_id,toverified_token)
-			if isvalid == false {log.Println(err.Error())}
 			return isvalid
 		},
 	}.Upgrade(writer, request, nil)
+
+	//生成conn连接结点对象
+	node := ConnNode{
+
+	}
+
 }
 
 func CheckToken(userId int64,token string) (bool,error) {
@@ -37,6 +50,5 @@ func CheckToken(userId int64,token string) (bool,error) {
 		return false,err
 	}
 	return true,nil
-
 }
 
--- InstantMessage/controller/init.go
+++ InstantMessage/controller/init.go
@@ -4,4 +4,3 @@ import "../service"
 
 var contactService  *service.ContactService
 var userService  *service.UserService
-//var chatService *service.ChatService
